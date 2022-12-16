5f5c2a7ff3511b3ac66092e51eb1e82c77df8852
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-12-16 07:28:53 -0300

    Improving the share mobile component

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,12 @@
 "use strict";
 const btn = document.querySelector(".btn");
-const tooltip = document.querySelector(".tooltip");
+const articleDesktop = document.querySelector(".article__desktop");
+const articleMobile = document.querySelector(".article__mobile");
+const articleMenu = document.querySelector(".article__menu");
 
 btn.addEventListener("click", function () {
    btn.classList.toggle("show");
+   articleDesktop.classList.toggle("hide");
+   articleMobile.classList.toggle("show");
+   articleMenu.classList.toggle("mobile");
 });
